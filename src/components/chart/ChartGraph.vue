eb3d8471f2b7b99499450e9489d48efe8d9b42ad
jyj <busy.joj@gmail.com>
2022-08-31 23:06:42 +0900

    국내현황 chart data 가져오기 작업

---
+++ src/components/chart/ChartGraph.vue
@@ -0,0 +1,75 @@
+<template>
+    <canvas width="400" height="400" ref="myChart"></canvas>
+</template>
+
+<script>
+import { Chart, registerables } from "chart.js"
+Chart.register(...registerables);
+
+export default {
+    name:'ChartGraph',
+    props:{
+        chartOptions:{
+            type : Object,
+            default(){
+                return{
+                    type:"bar",
+                    labels:["red","blus"],
+                    datasets:[
+                        {
+                            data:[12,19],
+                            label:"test",
+                            borderWidth:1,                       
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            options: {
+                                scales: {
+                                    y: {
+                                        beginAtZero: true
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+    },
+    data(){
+        return{
+            chartObject:null
+        }
+    },
+    mounted(){
+        this.drawChart()
+    },
+    methods:{
+        drawChart(){
+            const { type, datasets, labels, options } = this.chartOptions
+            const ctx = this.$refs.myChart.getContext('2d');
+            this.chartObject = new Chart(ctx, {
+                type: type,
+                data: {
+                    labels,
+                    datasets
+                },
+                options
+            });
+        }   
+    }    
+}
+</script>
+
+<style>
+
+</style>
--- src/components/corona/DomesticCases.vue
+++ src/components/corona/DomesticCases.vue
@@ -1,13 +1,62 @@
 <template>
   <h2>국내현황</h2>
+  <div>
+    <ul class="graph-list">
+      <li>
+        <h3>누적 확진/사망자 추이</h3>
+        <ChartGraph></ChartGraph>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import ChartGraph from '../chart/ChartGraph'
+import coronaMixin from '@/mixins/coronaMixin'
+import moment from 'moment'
+
 export default {
-    name:"DomesticCases"
+    name: "DomesticCases",
+    mixins:[coronaMixin],
+    components: { 
+      ChartGraph 
+    },
+    data(){
+      return{
+        caseKey:0,
+        compareKey:0,
+        monthDifference:6,
+        domesticData:[]
+      }
+    },
+    mounted(){
+      this.fetchDomestic()
+    },
+    methods:{
+      async fetchDomestic(){
+        const to = moment().format("YYYY-MM-DD")
+        const from = moment().subtract(this.monthDifference, "months").format("YYYY-MM-DD")
+        const params = {to, from }
+        const url = "https://api.covid19api.com/live/country/south-korea"
+        const domestic = await this.fetchData("get", url, params)
+        this.setDomesticData(domestic)
+      },
+      setDomesticData(domestic){
+        let pick = 0
+        const targetDates = []
+        while(pick < this.monthDifference){
+          targetDates.push(moment().subtract(pick, "months").utc().format().split("T")[0])
+          pick++
+        }
+        this.domesticData = domestic.filter(dom => targetDates.includes(dom.Date.split("T")[0]))
+        console.log(this.domesticData)
+      }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+.graph-list{
+  display:grid;grid-template-columns:repeat(3, 1fr);grid-gap:1em;list-style:none;
+}
 </style>
